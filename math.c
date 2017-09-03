4d0679561cf9d6d7b840b946140aefdbdd76ca5f
Ryan Scott <ryan.matt.scott@gmail.com>
2017-09-03 15:15:23 -0400

    Add math feature

---
+++ math.c
@@ -0,0 +1,18 @@
+#include "math.h"
+
+int add(int x, int y){
+    return x+y;
+}
+
+int sub(int x, int y){
+    return x-y;
+}
+
+int mult(int x, int y){
+    return x*y;
+}
+
+int div(int x, int y){
+   return x/y;
+}
+
---
+++ math.h
@@ -0,0 +1,12 @@
+#ifndef MATH_H_
+#define MATH_H_
+
+int add(int x, int y);
+
+int sub(int x, int y);
+
+int mult(int x, int y);
+
+int div(int x, int y);
+
+#endif
